e71cb1011dc80fd4ab82564d600016a8909f1dee
Sumit Yadav <sumii@Sumits-MacBook-Pro.local>
2024-11-19 22:44:04 +0530

    Portfolio-website

---
+++ style.js
@@ -0,0 +1,7 @@
+ const typed = new Typed('.multiple-text',{
+    strings: ["", 'Software Developer', 'Java Full Stack Developer'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true
+ });
